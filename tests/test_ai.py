55eb52ce4132d031252914ae4237bc09f228c9a1
lsommarberg <laura.sommarberg@gmail.com>
2024-02-15 13:27:25 +0200

    testit korjattu

--- tests/test_ai.py
+++ tests/test_ai.py
@@ -14,7 +14,7 @@ class TestGame(unittest.TestCase):
                  [" ", " ", "x"]]
         mock_tictactoe = Minimax(size=3)
         mock_tictactoe.board = board
-        moves = [(0, 1), (1, 2), (2, 0), (2, 1)]
+        moves = [(0, 1), (2, 1), (2, 0), (1, 2)]
         last_move = (2, 2)
         expected_move = (1, 2)
 
@@ -24,7 +24,8 @@ class TestGame(unittest.TestCase):
             alpha=-1000000,
             beta=1000000,
             moves=moves,
-            last_move=last_move
+            last_move=last_move,
+            count=5
         )
 
         self.assertEqual(actual_move, expected_move)
@@ -37,7 +38,7 @@ class TestGame(unittest.TestCase):
         
         mock_tictactoe = Minimax(size=3)
         mock_tictactoe.board = board
-        moves = [(0, 1), (1, 2), (2, 0), (2, 1)]
+        moves = [(2, 1), (1, 2), (2, 0), (0, 1)]
         last_move = (1, 1)
         expected_move = (0, 1)
         _, actual_move = mock_tictactoe.max_value(
@@ -46,7 +47,8 @@ class TestGame(unittest.TestCase):
             alpha=-1000000,
             beta=1000000,
             moves=moves,
-            last_move=last_move
+            last_move=last_move,
+            count=5
         )
 
         self.assertEqual(actual_move, expected_move)
